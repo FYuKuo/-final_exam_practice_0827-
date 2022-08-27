7fca17a86bcce79cec986022cf166f13fa3310f7
FY Kuo <teresa99339@gmail.com>
2022-08-27 13:10:28 +0800

    完成後台問卷顯示隱藏功能

--- back/que.php
+++ back/que.php
@@ -1,4 +1,4 @@
-<fieldset>
+<fieldset class="p-20">
     <legend>新增問卷</legend>
     <form action="./api/add_que.php" method="post">
     <div class="d-f">
@@ -19,11 +19,69 @@
     </form>
 </fieldset>
 
+<fieldset class="p-20">
+    <legend>問卷列表</legend>
+
+    <table class="w-90 ct aut">
+        <tr class="clo">
+            <td>問卷名稱</td>
+            <td style="width: 8%;">投票數</td>
+            <td style="width: 8%;">開放</td>
+        </tr>
+        <?php
+        $rows = $Que->all();
+        foreach ($rows as $key => $row) {
+        ?>
+        <tr>
+            <td>
+                <?=$row['text']?>
+            </td>
+            <td>
+                <?=$row['sum']?>
+            </td>
+            <td>
+                <?php
+                if($row['sh'] == 0){
+                ?>
+                <button type="button" onclick="sh(1,<?=$row['id']?>,this)">
+                    隱藏
+                </button>
+                <?php
+                }else{
+                ?>
+                <button type="button" onclick="sh(0,<?=$row['id']?>,this)">
+                    開放
+                </button>
+                <?php
+                }
+                ?>
+            </td>
+        </tr>
+        <?php
+        }
+        ?>
+    </table>
+</fieldset>
+
 
 <script>
     function more(){
         $('.moreDiv').prepend(`<div>選項<input type="text" name="opts[]" class="opts"></div>`)
     }
 
+    function sh(sh,id,e){
 
+        $.post('./api/sh.php',{sh,id},()=>{
+            console.log($(e));
+            if(sh == 0){
+                $(e).parent().html(`<button type="button" onclick="sh(1,${id},this)">
+                    隱藏
+                </button>`) 
+            }else{
+                $(e).parent().html(`<button type="button" onclick="sh(0,${id},this)">
+                開放
+                </button>`) 
+            }
+        })
+    }
 </script>
